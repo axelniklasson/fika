7b9456c6862551ddce7d7f494c94b2449ab5cb98
Axel Niklasson <axeln@uber.com>
2020-04-03 20:35:03 +0200

    Update health endpoint

--- server/index.js
+++ server/index.js
@@ -1,16 +1,24 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
 // constants
-const port = process.env.PORT || 3001
-const appName = 'FIKA'
+const port = process.env.PORT || 3001;
+const appName = "FIKA";
 
 // middleware
-app.use(cors())
+app.use(cors());
 
 // routes
-app.get('/', (req, res) => res.json({message: 'Hello from fika-server'}))
+app.get("/", (req, res) => {
+  revision = require("child_process")
+    .execSync("git rev-parse HEAD")
+    .toString()
+    .trim();
+  return res.json({ status: "healthy", hash: revision });
+});
 
 // launch app
-app.listen(port, '0.0.0.0', () => console.log(`${appName} listening at http://localhost:${port}`))
+app.listen(port, "0.0.0.0", () =>
+  console.log(`${appName} listening at http://localhost:${port}`)
+);
